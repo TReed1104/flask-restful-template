85dd7fb7cebf37499f03dcca89181a21207518b5
Tom Reed <t.reed1104@gmail.com>
2019-08-16 23:00:33 +0100

    Registered the example model

--- template.py
+++ template.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+from models import example_model
 from shared import db
 from resources import template_resource
 
