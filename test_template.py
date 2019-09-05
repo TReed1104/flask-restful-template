871160d7aab5715e4a37710de69297514cf89c3d
Tom Reed <t.reed1104@gmail.com>
2019-09-05 14:03:51 +0100

    Updated the templates test script to match the new style

--- test_template.py
+++ test_template.py
@@ -6,15 +6,15 @@ from template import app
 class Test_Resource_Template(unittest.TestCase):
     ## Setup Functions
     def setUp(self):
-        ## Take a copy of example.cfg and add the test database URI, or this will crash here
-        app.config.from_pyfile('testing.cfg')
+        app.config.from_pyfile('testing.cfg')   ## Take a copy of example.cfg and add the test database URI, or this will crash here
         self.app = app.test_client()
         with app.app_context():
-            db.drop_all()
             db.create_all()
-
+    ## Teardown
     def tearDown(self):
-        pass
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 ## Program main entry point
 if __name__ == '__main__':
