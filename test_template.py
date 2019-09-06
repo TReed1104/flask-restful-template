c1f8258d2f594944b7e6d8308c113db9e3b8bc0d
Tom Reed <t.reed1104@gmail.com>
2019-09-06 10:06:37 +0100

    Renamed the template test classes

--- test_template.py
+++ test_template.py
@@ -3,7 +3,20 @@ import unittest
 from shared import db
 from template import app
 
-class Test_Resource_Template(unittest.TestCase):
+class FeatureTest_Template(unittest.TestCase):
+    ## Setup Functions
+    def setUp(self):
+        app.config.from_pyfile('testing.cfg')   ## Take a copy of example.cfg and add the test database URI, or this will crash here
+        self.app = app.test_client()
+        with app.app_context():
+            db.create_all()
+    ## Teardown
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+class UnitTest_Template(unittest.TestCase):
     ## Setup Functions
     def setUp(self):
         app.config.from_pyfile('testing.cfg')   ## Take a copy of example.cfg and add the test database URI, or this will crash here
